4af3ed0cf74cb8317118e115ac8abdc752032312
Abdi Ahmed <89213120+abdirashidexe@users.noreply.github.com>
2025-09-28 23:26:26 -0700

    organized components

---
+++ src/MainContent.jsx
@@ -0,0 +1,12 @@
+export default function MainContent() {
+  return (
+    <main>
+      <h1>Reasons I'm excited to learn React</h1>
+      <ol>
+        <li>it's cool</li>
+        <li>higher pay</li>
+        <li>xD</li>
+      </ol>
+    </main>
+  )
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,47 +1,14 @@
 import { createRoot } from "react-dom/client";
+import WhateverComponent from "./Header.jsx" // showing it can be named whatever
+import MainContent from "./MainContent.jsx"
+import Footer from "./Footer.jsx"
 
 const root = createRoot(document.getElementById("root"));
-
-function Header() {
-  return(
-    <header className="myHeader">
-        <img src="src/assets/react.svg" alt="React logo" className="myLogo"/>
-        <nav>
-          <ul className="nav-list">
-            <li className="nav-list-item">Pricing</li>
-            <li className="nav-list-item">About</li>
-            <li className="nav-list-item">Contact</li>
-          </ul>
-        </nav>
-    </header>
-  )
-}
-
-function MainContent() {
-  return (
-    <main>
-      <h1>Reasons I'm excited to learn React</h1>
-      <ol>
-        <li>it's cool</li>
-        <li>higher pay</li>
-        <li>xD</li>
-      </ol>
-    </main>
-  )
-}
-
-function Footer() {
-  return (
-    <footer>
-      <small>© 2025 Ahmed development. All rights reserved.</small>
-    </footer>
-  )
-}
 
 function Page() {
   return (
     <>
-      <Header />
+      <WhateverComponent />
       <MainContent />
       <Footer />
     </>
