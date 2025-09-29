6d5f9986d327c0f1bcaca4ccbb7d8f452b61d1ba
Abdi Ahmed <89213120+abdirashidexe@users.noreply.github.com>
2025-09-28 23:19:03 -0700

    styling session

--- src/index.jsx
+++ src/index.jsx
@@ -2,28 +2,52 @@ import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
 
+function Header() {
+  return(
+    <header className="myHeader">
+        <img src="src/assets/react.svg" alt="React logo" className="myLogo"/>
+        <nav>
+          <ul className="nav-list">
+            <li className="nav-list-item">Pricing</li>
+            <li className="nav-list-item">About</li>
+            <li className="nav-list-item">Contact</li>
+          </ul>
+        </nav>
+    </header>
+  )
+}
+
+function MainContent() {
+  return (
+    <main>
+      <h1>Reasons I'm excited to learn React</h1>
+      <ol>
+        <li>it's cool</li>
+        <li>higher pay</li>
+        <li>xD</li>
+      </ol>
+    </main>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 2025 Ahmed development. All rights reserved.</small>
+    </footer>
+  )
+}
+
 function Page() {
   return (
     <>
-        <header>
-          <img src="src/assets/react.svg" alt="React logo"/>
-        </header>
-        <main>
-          <h1>Reasons I'm excited to learn React</h1>
-          <ol>
-            <li>it's cool</li>
-            <li>higher pay</li>
-            <li>xD</li>
-          </ol>
-        </main>
-        <footer>
-          <small>© 2025 Ahmed development. All rights reserved.</small>
-        </footer>
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   )
 }
 
 root.render(
-  //<MainContent />
   <Page />
 )
